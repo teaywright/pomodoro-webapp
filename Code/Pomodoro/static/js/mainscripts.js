6d98c4d2c8ac2c5bc27e8ff64c17b615ecb22b3c
JoshuaERichardson <joshuarichardson1478@gmail.com>
2021-10-05 19:27:08 -0600

    animated entrance of clock.  fixed animation of main container.  added horrible borders for development purposes only.  added temporary background.  added logo but did not convert to true png yet.

--- Code/Pomodoro/static/js/mainscripts.js
+++ Code/Pomodoro/static/js/mainscripts.js
@@ -5,24 +5,19 @@ console.log('Testing that we imported mainscripts.js correctly');
 //  Begin JS for main.html
 const work = document.getElementById("workSide");
 const chill = document.querySelector("chillSide");
+const mainContainer = document.getElementById("mainContainer")
 
 
 //Detect which side is dominant.  1 = working side dominant
 let detectSide = 1;
 function switchSides(){
     if(detectSide == 1){
-        console.log('inside of shrinkWork()');
-        work.classList.remove("animateGrowWork");
-        work.classList.add("animateShrinkWork");
-        chill.classList.remove("animateShrinkChill");
-        chill.classList.add("animateGrowChill");
+        mainContainer.classList.remove("animateGrowWork");
+        mainContainer.classList.add("animateShrinkWork");
         detectSide = 0;
     } else {
-        console.log('inside of shrinkChill()');
-        work.classList.remove("animateShrinkWork");
-        work.classList.add("animateGrowWork");
-        chill.classList.remove("animateGrowChill");
-        chill.classList.add("animateShrinkChill");
+        mainContainer.classList.add("animateGrowWork");
+        mainContainer.classList.remove("animateShrinkWork");
         detectSide = 1;
     }
 }
